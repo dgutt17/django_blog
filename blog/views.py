6d43f37321c261c195ce51fde7883473ba97c851
Dan Gutt <dgutt10@gmail.com>
2019-08-09 09:32:13 -0500

    Added some templates for the views

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        "author": "Dan",
+        'title': "Test",
+        "content": "Hello World!",
+        'date_posted': "Today"
+    },
+    {
+        "author": "Jane",
+        'title': "Test2",
+        "content": "Hello Moscow!",
+        'date_posted': "Yesterday"
+    },
+]
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
